9c5e80ad2ef10d973209475148766af5c4adc715
Keiji, Yoshimi <walf443@gmail.com>
2015-09-26 13:19:03 +0900

    deployできるようになった

--- go/deploy.sh
+++ go/deploy.sh
@@ -6,11 +6,11 @@ SSH_USER=isucon
 
 echo "deploy start by $USER" | ../notify_slack.sh
 
-for SSH_SERVER in isucon@test.asia-east1-c.isucon5-1060
+for SSH_SERVER in test.asia-east1-c.isucon5-1060
 do
   rsync -avz ./ $SSH_USER@$SSH_SERVER:/home/isucon/webapp/go/
   ssh -t $SSH_USER@$SSH_SERVER /bin/bash -c "/home/isucon/webapp/go/build.sh"
-  ssh -t $SSH_USER@$SSH_SERVER pkill app
+  ssh -t $SSH_USER@$SSH_SERVER sudo systemctl restart isuxi.go.service
 done
 
 echo "deploy finished $USER" | ../notify_slack.sh
