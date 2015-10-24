f484f46480d4c99852256d9a5b0ff44e9b0f2c65
Atsushi Takayama <taka.atsushi@gmail.com>
2015-10-24 12:30:13 +0900

    Revert "Revert "Merge branch 'cache-current-user'""
    This reverts commit 025fd870a8a468a33fde7c90c03b667f994c8e50.

--- go/app.go
+++ go/app.go
@@ -130,15 +130,11 @@ func getCurrentUser(w http.ResponseWriter, r *http.Request) *User {
 	if !ok || userID == nil {
 		return nil
 	}
-	row := db.QueryRow(`SELECT id, account_name, nick_name, email FROM users WHERE id=?`, userID)
-	user := User{}
-	err := row.Scan(&user.ID, &user.AccountName, &user.NickName, &user.Email)
-	if err == sql.ErrNoRows {
-		checkErr(ErrAuthentication)
+	userIDInt, ok := userID.(int)
+	if !ok {
+		return nil
 	}
-	checkErr(err)
-	context.Set(r, "user", user)
-	return &user
+	return getUser(w, userIDInt)
 }
 
 func authenticated(w http.ResponseWriter, r *http.Request) bool {
