5054d85094e35267aea820d7a2b6d89375d5f78a
Keiji, Yoshimi <walf443@gmail.com>
2015-10-24 10:56:03 +0900

    2台にdeployする

--- go/deploy.sh
+++ go/deploy.sh
@@ -4,13 +4,13 @@ set -x
 
 SSH_USER=isucon
 
-echo "deploy start by $USER" | ../notify_slack.sh
+# echo "deploy start by $USER" | ../notify_slack.sh
 
-for SSH_SERVER in test.asia-east1-c.isucon5-1060
+for SSH_SERVER in instance-1.asia-east1-c.isucon5-1060 instance-2.asia-east1-c.isucon5-1060
 do
   rsync -avz ./ $SSH_USER@$SSH_SERVER:/home/isucon/webapp/go/
   ssh -t $SSH_USER@$SSH_SERVER /bin/bash -c "/home/isucon/webapp/go/build.sh"
   ssh -t $SSH_USER@$SSH_SERVER sudo systemctl restart isuxi.go.service
 done
 
-echo "deploy finished $USER" | ../notify_slack.sh
+# echo "deploy finished $USER" | ../notify_slack.sh
